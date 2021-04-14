72a2435af7535a25f87d85cfd2c2fc15eed95f3b
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2021-04-14 12:36:04 +0100

    [PRM-117] - Initial repository setup and basic tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,11 +14,10 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "4.2.0"  % Test,
-    
     "org.scalatest"           %% "scalatest"                % "3.2.5"  % Test,
     "org.jsoup"               %  "jsoup"                    % "1.13.1" % Test,
     "com.typesafe.play"       %% "play-test"                % PlayVersion.current  % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
+    "com.vladsch.flexmark"     %  "flexmark-all"               % "0.35.10" % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"  % "test, it"
   )
 }
