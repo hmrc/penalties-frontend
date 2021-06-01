d32ef7cc64e0806c5a16fb516f3d04aec6b384d9
JBlacklawsTen10 <58850837+JBlacklawsTen10@users.noreply.github.com>
2021-06-01 15:09:33 +0100

    [PRM-253] Added Service and Spec

---
+++ test/services/ComplianceServiceSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import base.SpecBase
+import connectors.ComplianceConnector
+import models.compliance.CompliancePayload
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.{HeaderCarrier, UpstreamErrorResponse}
+
+import scala.concurrent.Future
+
+class ComplianceServiceSpec extends SpecBase {
+
+  val mocComplianceConnector: ComplianceConnector = mock(classOf[ComplianceConnector])
+
+  class Setup {
+    val service: ComplianceService = new ComplianceService(mocComplianceConnector)
+
+    reset(mocComplianceConnector)
+  }
+
+  "getComplianceDataWithVrn" should {
+    s"return a successful response and pass the result back to the controller" in new Setup {
+
+      when(mocComplianceConnector.getComplianceData(any())(any())).thenReturn(Future.successful(sampleComplianceData))
+
+      val result: CompliancePayload = await(service.getComplianceDataWithVrn(vrn)(HeaderCarrier()))
+
+      result shouldBe sampleComplianceData
+    }
+
+    s"return an exception and pass the result back to the controller" in new Setup {
+
+      when(mocComplianceConnector.getComplianceData(any())(any()))
+        .thenReturn(Future.failed(UpstreamErrorResponse.apply("Upstream error", INTERNAL_SERVER_ERROR)))
+
+      val result: Exception = intercept[Exception](await(service.getComplianceDataWithVrn(vrn)(HeaderCarrier())))
+
+      result.getMessage shouldBe "Upstream error"
+    }
+  }
+}
