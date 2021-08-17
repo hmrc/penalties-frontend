e3accf960232dce14ec22e0ad433b6a2e20757d3
“BackRowOla” <56401572+BackRowOla@users.noreply.github.com>
2021-08-17 16:32:19 +0100

    updated app and plugin dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,14 +6,14 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.3.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.64.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.12.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.94.0-play-28",
     "uk.gov.hmrc"             %% "play-frontend-govuk"        % "0.71.0-play-28",
-    "uk.gov.hmrc"             %% "play-language"              % "5.0.0-play-28"
+    "uk.gov.hmrc"             %% "play-language"              % "5.1.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"         % "5.3.0"            % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"         % "5.12.0"            % "test, it",
     "org.scalatest"           %% "scalatest"                      % "3.0.9"            % "test, it",
     "org.jsoup"               %  "jsoup"                          % "1.13.1"           % "test, it",
     "com.typesafe.play"       %% "play-test"                      % PlayVersion.current  % "test, it",
