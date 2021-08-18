4942892e58ee69f139898a0613bec9fc25f2818b
“BackRowOla” <56401572+BackRowOla@users.noreply.github.com>
2021-08-18 14:48:55 +0100

    switched from must to should matcher plus minor update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,8 +7,8 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.12.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.64.0-play-28",
-    "uk.gov.hmrc"             %% "play-frontend-govuk"        % "0.71.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.94.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-govuk"        % "0.84.0-play-28",
     "uk.gov.hmrc"             %% "play-language"              % "5.1.0-play-28"
   )
 
