3db2bf7c08239b248a35bee36f1d045bc47221fa
JBlacklawsTen10 <58850837+JBlacklaws@users.noreply.github.com>
2021-09-01 16:38:34 +0100

    [PRM-582] Added date calculation and new page variants for estimates

--- app/controllers/CalculationController.scala
+++ app/controllers/CalculationController.scala
@@ -16,11 +16,15 @@
 
 package controllers
 
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
 import config.{AppConfig, ErrorHandler}
 import controllers.predicates.AuthPredicate
 import models.penalty.LatePaymentPenalty
 import views.html.{CalculationAdditionalView, CalculationLPPView}
 import javax.inject.Inject
+import org.joda.time.Days
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import services.PenaltiesService
@@ -28,6 +32,7 @@ import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import utils.Logger.logger
 import utils.{CurrencyFormatter, EnrolmentKeys}
 import viewmodels.CalculationPageHelper
+import models.point.PointStatusEnum
 
 import scala.concurrent.ExecutionContext
 
@@ -66,7 +71,8 @@ class CalculationController @Inject()(viewLPP: CalculationLPPView, viewAdd: Calc
             )
           } else {
             val additionalPenaltyRate = "4"
-            Ok(viewAdd(additionalPenaltyRate))
+            val daysSince31 = ChronoUnit.DAYS.between(penalty.get.financial.dueDate, LocalDateTime.now())
+            Ok(viewAdd(daysSince31, penalty.get.status.equals(PointStatusEnum.Due), additionalPenaltyRate))
           }
         }
       }
