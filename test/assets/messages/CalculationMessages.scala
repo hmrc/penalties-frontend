b49d2c7a2e7e55e648a851961a43512277126865
Andrew Butcher <6122384+AndrewRButcher@users.noreply.github.com>
2022-02-17 11:35:00 +0000

    PRM-1082 Corrected service name in page title (#150)
    * PRM-1082 Corrected service name in page title
    
    * PRM-1082 Added title check in index view Spec

--- test/assets/messages/CalculationMessages.scala
+++ test/assets/messages/CalculationMessages.scala
@@ -17,9 +17,9 @@
 package assets.messages
 
 object CalculationMessages {
-  val titleLPP = "Late payment penalty - Penalties - GOV.UK"
+  val titleLPP = "Late payment penalty - Manage your VAT account - GOV.UK"
 
-  val titleAdditional = "Additional penalty - Penalties - GOV.UK"
+  val titleAdditional = "Additional penalty - Manage your VAT account - GOV.UK"
 
   val period = "1 October 2022 to 31 December 2022"
 
