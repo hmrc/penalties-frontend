ceeb3577af5bb4a5cf1292c9e777ef8655c51ad5
ruthdas03 <86231805+ruthdas03@users.noreply.github.com>
2022-05-30 15:37:01 +0100

    [PRM-1384] Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,13 +6,13 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "5.21.0",
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.8.0-play-28",
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "5.24.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.21.0-play-28",
     "uk.gov.hmrc"                  %% "play-frontend-govuk"        % "2.0.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"         % "5.21.0"             % "test, it",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"         % "5.24.0"             % "test, it",
     "org.jsoup"                    %  "jsoup"                          % "1.14.3"             % "test, it",
     "org.mockito"                  %  "mockito-all"                    % "1.10.19"            % "test, it"
   )
