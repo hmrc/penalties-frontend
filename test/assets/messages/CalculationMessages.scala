fb35fab7b9e18bd411d0af4dfc2adc58f53e4e2e
Ibtissamkaddach <115007313+Ibtissamkaddach@users.noreply.github.com>
2022-12-21 12:47:36 +0000

    [PRM-2120] added warning text component to Calculation page

--- test/assets/messages/CalculationMessages.scala
+++ test/assets/messages/CalculationMessages.scala
@@ -55,9 +55,9 @@ object CalculationMessages {
 
   val estimateFooterNoteBillPayment = "This penalty applies if VAT has not been paid for 15 days."
 
-  val estimateFooterNoteWarningTrader = "! The penalty will increase by a further 2% if VAT remains unpaid 30 days after the due date and you have not set up a payment plan."
+  val estimateFooterNoteWarningTrader = "! Warning: The penalty will increase by a further 2% if VAT remains unpaid 30 days after the due date and you have not set up a payment plan."
 
-  val estimateFooterNoteWarningAgent = "! The penalty will increase by a further 2% if VAT remains unpaid 30 days after the due date and your client has not set up a payment plan."
+  val estimateFooterNoteWarningAgent = "! Warning: The penalty will increase by a further 2% if VAT remains unpaid 30 days after the due date and your client has not set up a payment plan."
 
   val link = "Return to VAT penalties and appeals"
 
