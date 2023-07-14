d85061aedd738e2fb14d7e4cbdc1f029c3a57995
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2023-07-14 09:36:59 +0100

    [PRM-2731] - Uplifted dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion = "7.15.0"
+  lazy val bootstrapVersion = "7.19.0"
 
   val compile = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "7.3.0-play-28"
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "7.14.0-play-28"
   )
 
   val test = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"         % bootstrapVersion     % "test, it",
-    "org.jsoup"                    %  "jsoup"                          % "1.15.3"             % "test, it",
+    "org.jsoup"                    %  "jsoup"                          % "1.16.1"             % "test, it",
     "org.mockito"                  %  "mockito-all"                    % "1.10.19"            % "test, it"
   )
 }
