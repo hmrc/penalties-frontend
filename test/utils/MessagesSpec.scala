ba06905d371299b6d6295f27d89b2b3e001b0e87
JBlacklaws <58850837+JBlacklaws@users.noreply.github.com>
2023-10-18 15:19:38 +0100

    [PRM-3005] Removed rebasing error

--- test/utils/MessagesSpec.scala
+++ test/utils/MessagesSpec.scala
@@ -24,7 +24,7 @@ import scala.io.Source
 
 class MessagesSpec extends SpecBase {
   //TODO: remove "summaryCard.appealLink.aria.label.lpp.vatPaid" when translation is received
-  private val excludedKeys = Seq("service.homePageUrl", "common.pageTitle", "calculation.key.3.additional.text", "calculation.value.amount", "summaryCard.appealLink.aria.label.lpp.vatPaid", "calculation.p.1.30.days.lpp1")
+  private val excludedKeys = Seq("service.homePageUrl", "common.pageTitle", "calculation.key.3.additional.text", "calculation.value.amount", "summaryCard.appealLink.aria.label.lpp.vatPaid")
   private val MatchIncorrectTwoSingleQuotes = """\w+'{2}\w+""".r
   private val MatchIncorrectSingleQuote = """\w+'{1}\w+""".r
   private val MatchBacktickQuoteOnly = """`+""".r
