4784d9a7be5aa2ff69f916a4155e5cf7b9335f6d
AndrewRButcher <6122384+AndrewRButcher@users.noreply.github.com>
2023-11-23 08:58:16 +0000

    PRM-3114 Dependency & Plugin update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion = "7.22.0"
+  lazy val bootstrapVersion = "7.23.0"
 
   val compile = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "7.23.0-play-28"
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "7.29.0-play-28"
   )
 
   val test = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"         % bootstrapVersion     % "test, it",
-    "org.jsoup"                    %  "jsoup"                          % "1.16.1"             % "test, it",
+    "org.jsoup"                    %  "jsoup"                          % "1.16.2"             % "test, it",
     "org.mockito"                  %  "mockito-all"                    % "1.10.19"            % "test, it"
   )
 }
