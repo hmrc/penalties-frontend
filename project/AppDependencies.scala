b9940628e519b861ddbb5131282c2f5febf0488a
Roheel Arshad <48284720+RoheelArshad@users.noreply.github.com>
2024-01-02 14:08:12 +0000

    PRM-3173 upgraded to play 30 (#398)
    * PRM-3173 upgraded to play 30
    
    * PRM-3173 Update Auto build plugin
    
    ---------
    
    Co-authored-by: AndrewRButcher <6122384+AndrewRButcher@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion = "8.1.0"
+  lazy val bootstrapVersion = "8.2.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "7.29.0-play-28"
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"         % "8.0.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"         % bootstrapVersion     % "test, it",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"         % bootstrapVersion     % "test, it",
     "org.jsoup"                    %  "jsoup"                          % "1.16.2"             % "test, it",
     "org.mockito"                  %  "mockito-all"                    % "1.10.19"            % "test, it"
   )
