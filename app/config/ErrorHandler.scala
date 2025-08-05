439875798c3af5af63d6db694971f3f73f4bd5c9
Jacob Raffe <jakeraffe24@gmail.com>
2025-08-05 17:38:02 +0100

    [DL-17029] Upgrade bootstrap dependency

--- app/config/ErrorHandler.scala
+++ app/config/ErrorHandler.scala
@@ -16,29 +16,34 @@
 
 package config
 
-import javax.inject.{Inject, Singleton}
 import models.User
 import play.api.i18n.MessagesApi
 import play.api.mvc.Results.InternalServerError
-import play.api.mvc.{Request, Result}
+import play.api.mvc.{Request, RequestHeader, Result}
 import play.twirl.api.Html
 import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
 import views.html.ErrorTemplate
 import views.html.errors.InternalServerErrorCustom
 
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
 @Singleton
-class ErrorHandler @Inject()(errorTemplate: ErrorTemplate, iseCustom: InternalServerErrorCustom, val messagesApi: MessagesApi)(implicit appConfig: AppConfig)
+class ErrorHandler @Inject() (errorTemplate: ErrorTemplate, iseCustom: InternalServerErrorCustom, val messagesApi: MessagesApi)(implicit
+    appConfig: AppConfig,
+    val ec: ExecutionContext)
     extends FrontendErrorHandler {
 
-  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]): Html =
-    errorTemplate(pageTitle, heading, message)
-
-  def showInternalServerError(userOptional: Option[User[_]] = None)(implicit request: Request[_]): Result = {
-    if(userOptional.isDefined) {
-      implicit val user: User[_] = userOptional.get
-      InternalServerError(iseCustom())
-    } else {
-      InternalServerError(internalServerErrorTemplate)
-    }
+  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: RequestHeader): Future[Html] = {
+    implicit val req: Request[_] = request.asInstanceOf[Request[_]]
+    Future.successful(errorTemplate(pageTitle, heading, message))
   }
+
+  def showInternalServerError(userOptional: Option[User[_]] = None)(implicit request: Request[_]): Future[Result] =
+    if (userOptional.isDefined) {
+      implicit val user: User[_] = userOptional.get
+      Future.successful(InternalServerError(iseCustom()))
+    } else {
+      internalServerErrorTemplate.map(InternalServerError(_))
+    }
 }
