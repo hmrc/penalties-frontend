16d7a20d78f20ba7356e5cff2f2a2f69a3d85654
Jacob Raffe <jakeraffe24@gmail.com>
2025-09-10 20:18:38 +0100

    [DL-17029] Upgrade bootstrap dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  lazy val bootstrapVersion = "10.0.0"
+  lazy val bootstrapVersion = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.8.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.10.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30"  % bootstrapVersion % "test, it",
-    "org.jsoup"    % "jsoup"                   % "1.21.1"         % "test, it",
+    "org.jsoup"    % "jsoup"                   % "1.21.2"         % "test, it",
     "org.mockito" %% "mockito-scala-scalatest" % "2.0.0"          % "test, it",
-    "org.mockito"  % "mockito-core"            % "5.18.0"         % "test, it"
+    "org.mockito"  % "mockito-core"            % "5.19.0"         % "test, it"
   )
 }
