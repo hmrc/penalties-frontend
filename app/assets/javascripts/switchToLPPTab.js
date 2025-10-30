8e3cdf6562b3226a0a99b845bf1664e9fa67e473
Ali Shah <ali.shah@digital.hmrc.gov.uk>
2025-10-30 15:20:56 +0000

    Updated to match GOVVK v5 frontend

--- app/assets/javascripts/switchToLPPTab.js
+++ app/assets/javascripts/switchToLPPTab.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tabs = document.querySelectorAll('[data-module="govuk-tabs"]');
-    const tabsLib = new window.GOVUKFrontend.Tabs(tabs[0]);
-    window.GOVUKFrontend.Tabs.prototype.unhighlightTab(tabsLib.$tabs[0]);
-    window.GOVUKFrontend.Tabs.prototype.highlightTab(tabsLib.$tabs[1]);
-    document.getElementById('late-payment-penalties').classList.remove('govuk-tabs__panel--hidden');
-    document.getElementById('late-submission-penalties').classList.add('govuk-tabs__panel--hidden');
+    const lspTab = document.getElementById('lsp-tab');
+    const lppTab = document.getElementById('lpp-tab');
+    const lspTabLink = lspTab ? lspTab.querySelector('a.govuk-tabs__tab') : null;
+    const lppTabLink = lppTab ? lppTab.querySelector('a.govuk-tabs__tab') : null;
+    
+    const lspPanel = document.getElementById('late-submission-penalties');
+    const lppPanel = document.getElementById('late-payment-penalties');
+    
+    if (lspTab && lppTab && lspTabLink && lppTabLink && lspPanel && lppPanel) {
+        lspTab.classList.remove('govuk-tabs__list-item--selected');
+        lspTabLink.setAttribute('aria-selected', 'false');
+        lspTabLink.removeAttribute('tabindex');
+        
+        lppTab.classList.add('govuk-tabs__list-item--selected');
+        lppTabLink.setAttribute('aria-selected', 'true');
+        lppTabLink.setAttribute('tabindex', '0');
+        
+        lspPanel.classList.add('govuk-tabs__panel--hidden');
+        lppPanel.classList.remove('govuk-tabs__panel--hidden');
+    }
 })
