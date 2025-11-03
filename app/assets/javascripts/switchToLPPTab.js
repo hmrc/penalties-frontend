bbe3e9dc32e94fef47b0f8c75baf0980136e6ffc
Ali Shah <ali.shah@digital.hmrc.gov.uk>
2025-11-03 10:17:38 +0000

    fixed tab issues not rendering

--- app/assets/javascripts/switchToLPPTab.js
+++ app/assets/javascripts/switchToLPPTab.js
@@ -1,22 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const lspTab = document.getElementById('lsp-tab');
-    const lppTab = document.getElementById('lpp-tab');
-    const lspTabLink = lspTab ? lspTab.querySelector('a.govuk-tabs__tab') : null;
-    const lppTabLink = lppTab ? lppTab.querySelector('a.govuk-tabs__tab') : null;
-    
-    const lspPanel = document.getElementById('late-submission-penalties');
-    const lppPanel = document.getElementById('late-payment-penalties');
-    
-    if (lspTab && lppTab && lspTabLink && lppTabLink && lspPanel && lppPanel) {
-        lspTab.classList.remove('govuk-tabs__list-item--selected');
-        lspTabLink.setAttribute('aria-selected', 'false');
-        lspTabLink.removeAttribute('tabindex');
-        
-        lppTab.classList.add('govuk-tabs__list-item--selected');
-        lppTabLink.setAttribute('aria-selected', 'true');
-        lppTabLink.setAttribute('tabindex', '0');
-        
-        lspPanel.classList.add('govuk-tabs__panel--hidden');
-        lppPanel.classList.remove('govuk-tabs__panel--hidden');
+// Switch to LPP tab when there are no LSP cards
+window.addEventListener('load', function() {
+    const lppTabLink = document.querySelector('#lpp-tab a');
+    if (lppTabLink) {
+        lppTabLink.click();
     }
-})
+});
