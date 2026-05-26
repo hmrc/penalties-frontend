157a2368567c7dd79c1ad9626e93cffc21a655a3
kannanekambaram <112036677+kannanEkambaram@users.noreply.github.com>
2026-05-26 09:54:53 +0100

    DL-18823: Update ready for nav banner change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.10.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.31.0"
   )
 
   val test: Seq[ModuleID] = Seq(
